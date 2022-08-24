20ba8010a9f4963e607fb046a33b53f829292fb1
Maria Helena <mhbianchi1@gmail>
2022-08-24 00:03:47 -0300

    Alteração do base Path

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -18,7 +18,7 @@ public class ProdutoTest {
     public void beforeEach(){
         //Configurando os dados da API Rest da Lojinha
         baseURI = "http://165.227.93.41";
-        basePath = "/lojinha-bugada";
+        basePath = "/lojinha";
 
         //Obter o token do usuário admin
         this.token = given()
